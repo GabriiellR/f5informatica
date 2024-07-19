274a3e293566dd78faa0572e411e26b5d5484d30
Gabriel Dias <desenvolvimento01@somalogistica.com.br>
2024-07-19 00:09:05 -0300

    Feat: Desenvolvimento da pagina sobrenos, necessário ajustes

--- src/Paginas/Sobrenos/Sobrenos.jsx
+++ src/Paginas/Sobrenos/Sobrenos.jsx
@@ -1,11 +1,15 @@
+import Timeline from "../../Componentes/Timeline/Timeline.jsx";
 import TransicaoPagina from "../../Componentes/TransicaoPagina/TransicaoPagina"
 
 const Sobrenos = (() => {
     return (<>
 
 
-        <div className="min-h-screen bg-gradient-to-r from-corSecundaria  to-corPrimaria text-corFonte ">
-            <div>
+        <div className="min-h-screen bg-Sobrenos bg-center bg-no-repeat bg-cover">
+           
+           <Timeline />
+           
+            {/* <div>
                 A F5 Informática foi fundada em 24 de novembro de 2003, na regional Vargem das Flores, em Contagem, estado de Minas Gerais, emergindo de um contexto sócio-econômico desafiador, comum a grande parte da população brasileira. Ao longo dos anos, enfrentamos dificuldades e superamos obstáculos, crescendo apesar das adversidades enfrentadas.
                 Desde o início de nossa trajetória, nosso foco tem sido em fornecer soluções tecnológicas e assistência às pessoas. Desde tenra idade, dedicamo-nos à prestação de serviços de manutenção de computadores, venda de suprimentos e periféricos de tecnologia, operação de LanHouse, oferta de cursos profissionalizantes e uma gama diversificada de serviços na área de tecnologia. Hoje, com 18 anos de atuação, estamos completamente imersos no mundo da tecnologia, destacando-nos não apenas por nossos serviços, mas também pelo impacto social positivo que geramos em Nova Contagem e nos bairros circunvizinhos.
 
@@ -17,7 +21,7 @@ const Sobrenos = (() => {
                 Ao longo dos últimos 18 anos, temos realizado mais de 5 mil atendimentos diversos por mês, marcando um impacto significativo na comunidade. Durante esse período, testemunhamos uma transformação digital próxima às residências das pessoas, proporcionando acesso à tecnologia. Nossos cursos têm sido fundamentais na preparação dos participantes para o mercado de trabalho, focando na profissionalização e sendo constantemente atualizados de acordo com as demandas do mercado. Orgulhamo-nos de ter formado e capacitado 6.280 indivíduos, preparando-os para enfrentar entrevistas de emprego com confiança.
 
                 A F5 Informática sempre priorizou a satisfação do cliente, esforçando-se para oferecer serviços de alta qualidade. Além disso, mantivemos uma parceria frutífera com o programa "Fica Vivo" ao longo de 10 anos, proporcionando assistência a jovens em situação de vulnerabilidade social. Reconhecida pelo SEBRAE com a nota nove como um empreendimento exemplar, tornamo-nos objeto de estudo como um caso de sucesso e uma referência regional pela UFMG.
-            </div>
+            </div> */}
         </div>
     </>)
 })
